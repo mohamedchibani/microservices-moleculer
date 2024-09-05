6c3e916e0c2ede7ac3049a5701ad5f72359c940d
Mohamed CHIBANI <41089034+mohamedchibani@users.noreply.github.com>
2024-09-06 00:03:06 +0100

    auth service

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 import UserService from "./services/user.service.js";
+import EmailService from "./services/email.service.js";
+import AuthService from "./services/auth.service.js";
 
 async function startApp() {
   // start services
   await UserService.start();
+  await EmailService.start();
+  await AuthService.start();
 
   try {
     // simulate user creation
     const newUser = await UserService.call("user.createUser", {
-      username: "mchibani",
+      username: "admin",
       email: "mohamed.chibani@email.com",
     });
     console.log("New User Created", newUser);
     const users = await UserService.call("user.getUsers");
     console.log("All users", users);
+
+    // simulate sending email
+    const emailResult = await EmailService.call("email.sendEmail", {
+      recipient: newUser.email,
+      subject: "Welcome to the plateform!",
+      content: "thank you for signing up",
+    });
+    console.log(emailResult);
+
+    // simulate auth
+    const authResult = await AuthService.call("auth.authUser", {
+      username: newUser.username,
+      password: "password",
+    });
+
+    console.log("Auth result:", authResult);
   } catch (error) {
     console.error("Error:", error);
   } finally {
     await UserService.stop();
+    await EmailService.stop();
+    await AuthService.stop();
   }
 }
 
