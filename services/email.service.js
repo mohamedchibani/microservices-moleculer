9239ddd30efe1b7dbf7385f893d0bbe586d1312c
Mohamed CHIBANI <41089034+mohamedchibani@users.noreply.github.com>
2024-09-05 23:57:29 +0100

    email service

---
+++ services/email.service.js
@@ -0,0 +1,20 @@
+import { ServiceBroker } from "moleculer";
+
+const broker = new ServiceBroker();
+
+broker.createService({
+  name: "email",
+  actions: {
+    async sendEmail(ctx) {
+      const { recipient, subject, content } = ctx.params;
+
+      // sumulated email logic
+      console.log(`Sending email to ${recipient} with subject ${subject}`);
+      console.log(`Content ${content}`);
+
+      return `Email sent to ${recipient}`;
+    },
+  },
+});
+
+export default broker;
